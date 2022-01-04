c29fd96e31dd8c1834e7bb1f3c973a40d44fef94
Petre-Marian Vaduva <rik_296@yahoo.com>
2022-01-04 22:08:16 +0000

    create new class Family

--- bookstore/book_outlet/models.py
+++ bookstore/book_outlet/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.db.models.base import Model
 from django.db.models.fields import CharField, IntegerField
 
 # Create your models here.
 
 class Book(models.Model):
     title = models.CharField(max_length=50)
-    rating = models.IntegerField
+    my_rating = models.IntegerField()
+
+class pets(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField()
+
+
+class Family(models.Model):
+    name = models.CharField(max_length=50)
+    age = models.IntegerField() 
