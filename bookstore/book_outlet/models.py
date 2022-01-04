a937de41c197fc7a37377c4d3fc52e4fd5c6064b
Petre-Marian Vaduva <rik_296@yahoo.com>
2022-01-04 00:24:21 +0000

    create a Django Model with fields

--- bookstore/book_outlet/models.py
+++ bookstore/book_outlet/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.fields import CharField, IntegerField
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    rating = models.IntegerField
