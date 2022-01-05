6b19c6cf59a34702f75c4a2b10600cf47909573c
Petre-Marian Vaduva <rik_296@yahoo.com>
2022-01-05 06:33:20 +0000

    render Family from database to index.html

--- bookstore/book_outlet/views.py
+++ bookstore/book_outlet/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from .models import Family
 # Create your views here.
 
 def index(request):
-    return render(request, 'book_outlet/index.html')
+    members = Family.objects.all()
+    return render(request, 'book_outlet/index.html', {
+        'members': members
+    })
