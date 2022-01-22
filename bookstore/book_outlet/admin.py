10295852acef9d4a142df184db0265f17aff0a19
Petre-Marian Vaduva <rik_296@yahoo.com>
2022-01-22 15:29:41 +0000

    add Country model

--- bookstore/book_outlet/admin.py
+++ bookstore/book_outlet/admin.py
@@ -1,14 +1,18 @@
 from atexit import register
 from os import name
 from django.contrib import admin
-from .models import Book, Name
+from .models import Address, Book, Country, Name
 # Register your models here.
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('name', 'title')
 
-
+@admin.register(Name)
 class NameAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name')
-admin.site.register(Name, NameAdmin)
+
+
+
+admin.site.register(Country)
+admin.site.register(Address)
--- bookstore/book_outlet/models.py
+++ bookstore/book_outlet/models.py
@@ -1,3 +1,4 @@
+from tabnanny import verbose
 from tkinter import CASCADE
 from django.core import validators
 from django.db import models
@@ -8,11 +9,20 @@ from django.core.validators import MinLengthValidator, MaxLengthValidator
 
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    code = IntegerField()
+
+
 class Address(models.Model):
     street = CharField(max_length=50)
     postal_code = IntegerField()
     def __str__(self):
         return f'{self.street} {self.postal_code}'
+
+    class Meta:
+        verbose_name_plural = "Address entries"
+
 
 class Name(models.Model):
     first_name = models.CharField(max_length=50)
@@ -26,6 +36,7 @@ class Name(models.Model):
 class Book(models.Model):
     name = models.ForeignKey(Name, on_delete=models.CASCADE, null=True)
     title = models.CharField(max_length=50)
+    published_countries = models.ManyToManyField(Country)
 
     def __str__(self):
         return f'{self.name} ({self.title})'
