b07b77f9b41b3127fdceec5c5bf81e9e6d34aa30
Petre-Marian Vaduva <rik_296@yahoo.com>
2022-01-23 18:47:09 +0000

    update admin models

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -15,11 +15,11 @@ class Author(models.Model):
     email_address = models.EmailField()
 
 class Post(models.Model):
-    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='posts')
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
     title = models.CharField(max_length=50)
     Excerpt = models.CharField(max_length=200)
     image_name = models.CharField(max_length=50)
     date = models.DateField(auto_now=True)
     content = models.TextField()
-    tag = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag)
 
