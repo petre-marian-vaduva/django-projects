693e7fc9bd04dcf20cc4103937a47b4ff81c0647
Petre-Marian Vaduva <rik_296@yahoo.com>
2022-01-23 19:44:11 +0000

    update Author model

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,4 +1,5 @@
 from email import charset
+from logging import captureWarnings
 from sre_constants import SRE_INFO_LITERAL
 from tkinter import CASCADE
 from django.db import models
@@ -9,10 +10,20 @@ from datetime import datetime
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
+    def __str__(self) -> str:
+        return self.caption
+
 class Author(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     email_address = models.EmailField()
+    
+    def full_name(self):
+        return f'{self.first_name} {self.last_name} {self.email_address}'
+
+    def __str__(self):
+        return self.full_name()
+
 
 class Post(models.Model):
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, null=True, related_name='posts')
