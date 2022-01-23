1ab3646a2b311adf168b366d6e0895b662893bc2
Petre-Marian Vaduva <rik_296@yahoo.com>
2022-01-23 18:31:50 +0000

    add Author model and Post model

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -1,3 +1,21 @@
+from email import charset
+from sre_constants import SRE_INFO_LITERAL
+from tkinter import CASCADE
 from django.db import models
-
+from django.forms import CharField
+from datetime import datetime
 # Create your models here.
+
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    email_address = models.EmailField()
+
+class Post(models.Model):
+    author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='posts')
+    title = models.CharField(max_length=50)
+    Excerpt = models.CharField(max_length=200)
+    image_name = models.CharField(max_length=50)
+    date = models.DateField(auto_now=True)
+    content = models.TextField()
+
