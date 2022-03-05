9d918871e6e6b4e10458901ee019f36293245784
Petre-Marian Vaduva <rik_296@yahoo.com>
2022-03-05 23:43:39 +0000

    create new form

--- to_do/to_do_project/to_do/views.py
+++ to_do/to_do_project/to_do/views.py
@@ -1,14 +1,28 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from to_do.forms import TaskForm
+from .models import Task
 
 # Create your views here.
 
 def index(request):
+    tasks = Task.objects.all()
     form = TaskForm()
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        print(request.POST)
+        if form.is_valid():
+            form.save()
+        return HttpResponseRedirect('/')
+
     return render(request, 'to_do/index.html', {
-        'form': form
+        'form': form,
+        'tasks': tasks
     })
 
-def test(request):
-    return render(request, 'to_do/test.html')
+def test(request, number):
+    row = Task.objects.get(pk=number)
+    form = TaskForm(instance=row)
+    return render(request, 'to_do/test.html', {
+        'form': form
+    })
